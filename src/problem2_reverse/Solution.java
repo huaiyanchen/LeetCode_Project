19c4fc641c372df2506bbbbe8210664a22453ff8
chy <18712302931@163.com>
2021-03-21 22:41:09 +0800

    整数反转

---
+++ src/problem2_reverse/Solution.java
@@ -0,0 +1,38 @@
+package problem2_reverse;
+
+
+public class Solution {
+    public static  int reverse(int x) {
+        String s = String.valueOf(x);
+        String s2 = new StringBuffer(s).reverse().toString();
+        return Integer.parseInt(s2);
+    }
+    public static  int reverse2(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            //判断  rev = rev * 10 + pop 这句会不会溢出,就是判断0x80000000 /10 < rev < 0x7fffffff
+            //自己疑惑的点 1 :  7和-8
+            //              通过debug发现 / 会导致最后一位的判断会导致有可能int类型的溢出
+            //              int的取值范围是 - 2 *  31~ 2 * 31 - 1
+             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
+            {return 0;}
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
+            { return 0;}
+
+            rev = rev * 10 + pop;
+        }
+        return rev;
+
+    }
+
+    public static void main(String[] args) {
+        int x = 2147483647;
+//        System.out.println("reverse(x) = " + reverse(x));
+        System.out.println("reverse2(x) = " + reverse2(x));
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
+
+    }
+}
